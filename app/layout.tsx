cec95154da45e16ac4fbeb36ccc8e2701ba535c9
Sunghyun Hwang <hwang@hey.com>
2024-02-04 15:34:24 +0900

    Design layout (#18)
    * Configure layout
    
    Signed-off-by: Sunghyun Hwang <hwang@hey.com>
    
    * Extract
    
    Signed-off-by: Sunghyun Hwang <hwang@hey.com>
    
    * Add shadcn/ui
    
    Signed-off-by: Sunghyun Hwang <hwang@hey.com>
    
    * Fix scrolling
    
    Signed-off-by: Sunghyun Hwang <hwang@hey.com>
    
    * Add copyright
    
    Signed-off-by: Sunghyun Hwang <hwang@hey.com>
    
    * Add social links
    
    Signed-off-by: Sunghyun Hwang <hwang@hey.com>
    
    * Group essays by year
    
    Signed-off-by: Sunghyun Hwang <hwang@hey.com>
    
    ---------
    
    Signed-off-by: Sunghyun Hwang <hwang@hey.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 
 import './globals.css';
+import { Nav } from './nav';
 
 export const runtime = 'edge';
 
@@ -18,8 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>
-        {children}
+      <body className="flex flex-col-reverse overflow-y-scroll md:max-h-dvh md:flex-row md:overflow-hidden">
+        <Nav className="shrink-0 grow-0 basis-auto p-10 md:max-h-dvh md:overflow-y-scroll md:border-r md:border-r-accent" />
+        <main className="grow p-10 shadow-lg md:max-h-dvh md:overflow-y-scroll">
+          {children}
+        </main>
         <Analytics />
       </body>
     </html>
