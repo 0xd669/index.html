410ed834186ecd7711fb77c5d4094e3e3c821bf0
Sunghyun Hwang <hwang@hey.com>
2024-04-20 10:42:13 +0900

    Cleanup
    Signed-off-by: Sunghyun Hwang <hwang@hey.com>

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,7 @@ import BundleAnalyzerPlugin from '@next/bundle-analyzer';
 import { withContentlayer } from 'next-contentlayer2';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'fonts.gstatic.com',
-        port: '',
-        pathname: '/s/e/notoemoji/latest/**',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 const withBundleAnalyzer = BundleAnalyzerPlugin({
   enabled: process.env.ANALYZE === 'true',
