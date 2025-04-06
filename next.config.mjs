164c906de1377e04e067fb45b5116a5499b3e3bc
Sunghyun Hwang <hwang@hey.com>
2025-04-06 20:09:30 +0900

    Cleanup
    Signed-off-by: Sunghyun Hwang <hwang@hey.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
-import BundleAnalyzerPlugin from '@next/bundle-analyzer';
-import { withContentlayer } from 'next-contentlayer2';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const withBundleAnalyzer = BundleAnalyzerPlugin({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-export default withBundleAnalyzer(withContentlayer(nextConfig));
+export default nextConfig;
