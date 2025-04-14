a1ab8e38b094b90745a80550f3f85740b77dd8a3
Sunghyun Hwang <hwang@hey.com>
2025-04-14 22:53:39 +0900

    Implement favorites concept
    Signed-off-by: Sunghyun Hwang <hwang@hey.com>

--- app/(index)/favorites.tsx
+++ app/(index)/favorites.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { Dice1, Dice2, Dice3, Dice4, Dice5, Dice6 } from 'lucide-react';
 import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
 
 const texts = ['1', '2', '3', '4', '5', '6'];
 
@@ -10,10 +13,23 @@ export function Favorites() {
   );
 
   return (
-    <div className="flex flex-col gap-4">
-      <article className="text-base leading-relaxed">
+    <div className="flex flex-col gap-8">
+      <article className="text-muted-foreground text-base leading-relaxed">
         {texts[diceNumber]}
       </article>
+      <Button
+        variant="ghost"
+        onClick={() => setDiceNumber(Math.floor(Math.random() * texts.length))}
+        size="icon"
+        className="self-end"
+      >
+        {diceNumber === 0 && <Dice1 className="size-4" />}
+        {diceNumber === 1 && <Dice2 className="size-4" />}
+        {diceNumber === 2 && <Dice3 className="size-4" />}
+        {diceNumber === 3 && <Dice4 className="size-4" />}
+        {diceNumber === 4 && <Dice5 className="size-4" />}
+        {diceNumber === 5 && <Dice6 className="size-4" />}
+      </Button>
     </div>
   );
 }
